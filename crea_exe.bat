c91e131cd29b52bf02ff2159faa49536962d52fb
Gianluca <g.bertelli@orion-srl.it>
2025-10-14 11:20:22 +0200

    Saved

--- crea_exe.bat
+++ crea_exe.bat
@@ -1,3 +1,3 @@
 rem pyinstaller --onefile --noconsole main.py
 
-pyinstaller --onefile --name SendHttpsPostgres.exe main.py
+pyinstaller --onefile --noconsole --name SendHttpsPostgres.exe main.py
--- main_01497.bat
+++ main_01497.bat
@@ -1,6 +1,8 @@
 @echo off
 
 rem python main.py --sigla_staz "01497"
-.\SendHttpsPostgres.exe --sigla_staz "01497"
+rem .\SendHttpsPostgres.exe --sigla_staz "01497"
+
+start "" "C:\Dati_ETL\SendHttpsPostgres\SendHttpsPostgres.exe" --sigla_staz 01497
 
 exit
